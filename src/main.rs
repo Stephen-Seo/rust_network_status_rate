5e84030cf447a360f8ef8ff865b5ae18ce793c03
Stephen Seo <seo.disparate@gmail.com>
2022-02-07 17:54:58 +0900

    Finish impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::env::var;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::ops::Sub;
+use std::path::{Path, PathBuf};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
+use structopt::StructOpt;
+
+const INTERVAL_SECONDS: u64 = 5;
+
+#[derive(StructOpt, Debug, Clone)]
+struct Opt {
+    net_dev: String,
+}
 
 #[derive(Copy, Clone, Debug)]
 struct ByteState {
@@ -41,7 +51,7 @@ fn read_proc_net_dev(net_device: &str) -> Result<ByteState, String> {
     for line in file_string.lines() {
         if line.trim().starts_with(net_device) {
             let mut count = 0u32;
-            for word in line.split(' ') {
+            for word in line.split_ascii_whitespace() {
                 if count == 1 {
                     byte_state.recv = u64::from_str_radix(word, 10).map_err(|_| {
                         String::from("Failed to parse recv bytes from \"/proc/net/dev\"")
@@ -66,8 +76,8 @@ fn read_proc_net_dev(net_device: &str) -> Result<ByteState, String> {
 
 fn write_compare_state(
     net_device: &str,
-    send_filename: &str,
-    recv_filename: &str,
+    send_filename: &Path,
+    recv_filename: &Path,
 ) -> Result<ByteState, String> {
     let mut prev_byte_state = ByteState { send: 0, recv: 0 };
 
@@ -102,21 +112,48 @@ fn write_compare_state(
 
     {
         let mut send_file = File::create(send_filename)
-            .map_err(|_| format!("Failed to create \"{}\"", send_filename))?;
+            .map_err(|_| format!("Failed to create \"{:?}\"", send_filename))?;
         send_file
             .write_all(byte_state.send.to_string().as_bytes())
-            .map_err(|_| format!("Failed to write into \"{}\"", send_filename))?;
+            .map_err(|_| format!("Failed to write into \"{:?}\"", send_filename))?;
     }
 
     {
         let mut recv_file = File::create(recv_filename)
-            .map_err(|_| format!("Failed to create \"{}\"", recv_filename))?;
+            .map_err(|_| format!("Failed to create \"{:?}\"", recv_filename))?;
         recv_file
             .write_all(byte_state.recv.to_string().as_bytes())
-            .map_err(|_| format!("Failed to write into \"{}\"", recv_filename))?;
+            .map_err(|_| format!("Failed to write into \"{:?}\"", recv_filename))?;
     }
 
     Ok(byte_state - prev_byte_state)
+}
+
+fn do_set_states(
+    net_device: &str,
+    send_interval_filename: &Path,
+    recv_interval_filename: &Path,
+    send_total_filename: &Path,
+    recv_total_filename: &Path,
+) -> Result<(), String> {
+    let state = write_compare_state(net_device, send_total_filename, recv_total_filename)?;
+
+    {
+        let mut send_interval_file = File::create(send_interval_filename)
+            .map_err(|_| format!("Failed to create \"{:?}\"", send_interval_filename))?;
+        send_interval_file
+            .write_all(state.send.to_string().as_bytes())
+            .map_err(|_| format!("Failed to write into \"{:?}\"", send_interval_filename))?;
+    }
+    {
+        let mut recv_interval_file = File::create(recv_interval_filename)
+            .map_err(|_| format!("Failed to create \"{:?}\"", recv_interval_filename))?;
+        recv_interval_file
+            .write_all(state.recv.to_string().as_bytes())
+            .map_err(|_| format!("Failed to write into \"{:?}\"", recv_interval_filename))?;
+    }
+
+    Ok(())
 }
 
 fn timer_execute<F>(func: F, sleep_seconds: u64) -> Result<(), String>
@@ -135,6 +172,45 @@ where
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), String> {
+    let opt = Opt::from_args();
+
+    println!("Using net_dev == \"{}\"", opt.net_dev);
+
+    let xdg_runtime_dir = var("XDG_RUNTIME_DIR").map_err(|e| format!("{}", e))?;
+
+    let mut send_total_path = PathBuf::new();
+    send_total_path.push(&xdg_runtime_dir);
+    send_total_path.push("rust_send_total");
+    let send_total_path = send_total_path;
+
+    let mut recv_total_path = PathBuf::new();
+    recv_total_path.push(&xdg_runtime_dir);
+    recv_total_path.push("rust_recv_total");
+    let recv_total_path = recv_total_path;
+
+    let mut send_interval_path = PathBuf::new();
+    send_interval_path.push(&xdg_runtime_dir);
+    send_interval_path.push("rust_send_interval");
+    let send_interval_path = send_interval_path;
+
+    let mut recv_interval_path = PathBuf::new();
+    recv_interval_path.push(&xdg_runtime_dir);
+    recv_interval_path.push("rust_recv_interval");
+    let recv_interval_path = recv_interval_path;
+
+    timer_execute(
+        move || {
+            do_set_states(
+                &opt.net_dev,
+                &send_interval_path,
+                &recv_interval_path,
+                &send_total_path,
+                &recv_total_path,
+            )
+        },
+        INTERVAL_SECONDS,
+    )?;
+
+    Ok(())
 }
